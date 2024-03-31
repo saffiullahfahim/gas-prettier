6115ed2971007b914ec9ec3edd03de31c4bda7cc
saffiullahfahim <saffiullah.fahim@gmail.com>
2024-03-31 20:17:36 +0600

    v1

--- README.md
+++ README.md
@@ -1,35 +1,3 @@
-# GAS Extension
+# GAS Prettier
 
-This is for GAS (Google Apps Script) extension where you can add prettier formatter. And save the file with prettier format and locally.
-
-## Prerequisite
-script need for paste the console
-```js
-const fetchLink = "https://saffiullahfahim.github.io/gas-extension/src/loadScript.js";
-
-try {
-  window.trustedTypes.createPolicy("default", {
-    createHTML: (string) => string, // warning: this is unsafe!
-    createScriptURL: (string) => string, // warning: this is unsafe!
-    createScript: (string) => string, // warning: this is unsafe!
-  });
-} catch (err) {
-  console.log(err);
-}
-
-fetch(fetchLink)
-  .then((response) => {
-    return response.text();
-  })
-  .then((data) => {
-    let script = document.createElement("script");
-    script.innerHTML = data;
-    document.body.appendChild(script);
-  });
-```
-
-## Features
-- [x] prettier format
-- [x] save the file with prettier format
-- [ ] save file locally or remotely 
-- [ ] push the file to github
+Google Apps Script Prettier Formatter Chrome Extention
--- src/loadScript.js
+++ src/loadScript.js
@@ -1,58 +1,57 @@
 const __script = async () => {
-  /**
-   * Fetches prettier script and adds as script tag to the document body
-   * Fetches babel script and adds as script tag to the document body
-   */
-
-  if (window.prettier) return;
-
-  let prettierScriptResponse = await (
-    await fetch("https://unpkg.com/prettier@2.8.7/standalone.js")
-  ).text();
-
-  let babelScriptResponse = await (
-    await fetch("https://unpkg.com/prettier@2.8.7/parser-babel.js")
-  ).text();
-
-  const script = document.createElement("script");
-  script.innerHTML =
-    prettierScriptResponse.replace(
-      `if(typeof exports=="object"&&typeof module=="object")module.exports=e();else if(typeof define=="function"&&define.amd)define(e);else{var f=typeof globalThis<"u"?globalThis:typeof global<"u"?global:typeof self<"u"?self:this||{};f.prettier=e()}`,
-      "window.prettier=e()"
-    ) +
-    babelScriptResponse.replace(
-      `if(typeof exports=="object"&&typeof module=="object")module.exports=e();else if(typeof define=="function"&&define.amd)define(e);else{var i=typeof globalThis<"u"?globalThis:typeof global<"u"?global:typeof self<"u"?self:this||{};i.prettierPlugins=i.prettierPlugins||{},i.prettierPlugins.babel=e()}`,
-      "window.prettierPlugins=window.prettierPlugins||{},window.prettierPlugins.babel=e()"
-    );
-  document.body.insertBefore(script, document.body.lastChild);
-
   // add custom formattor for monaco editor
-  const prettierFormat = (value) => {
+  const prettierFormat = (value, type) => {
     let prettierConfig = {
-      arrowParens: "always",
-      bracketSameLine: false,
-      bracketSpacing: true,
-      embeddedLanguageFormatting: "auto",
-      htmlWhitespaceSensitivity: "css",
-      insertPragma: false,
-      jsxSingleQuote: false,
-      printWidth: 80,
-      proseWrap: "preserve",
-      quoteProps: "as-needed",
-      requirePragma: false,
-      semi: true,
-      singleAttributePerLine: false,
-      singleQuote: false,
-      tabWidth: 2,
-      trailingComma: "es5",
-      useTabs: false,
-      vueIndentScriptAndStyle: false,
-      parser: "babel",
-      prettierVersion: "2.8.7",
-      plugins: prettierPlugins,
+      js: {
+        arrowParens: "always",
+        bracketSameLine: false,
+        bracketSpacing: true,
+        embeddedLanguageFormatting: "auto",
+        htmlWhitespaceSensitivity: "css",
+        insertPragma: false,
+        jsxSingleQuote: false,
+        printWidth: 80,
+        proseWrap: "preserve",
+        quoteProps: "as-needed",
+        requirePragma: false,
+        semi: true,
+        singleAttributePerLine: false,
+        singleQuote: false,
+        tabWidth: 2,
+        trailingComma: "es5",
+        useTabs: false,
+        vueIndentScriptAndStyle: false,
+        parser: "babel",
+        prettierVersion: "2.8.7",
+        plugins: prettierPlugins,
+      },
+      html: {
+        arrowParens: "always",
+        bracketSameLine: false,
+        bracketSpacing: true,
+        semi: true,
+        experimentalTernaries: false,
+        singleQuote: false,
+        jsxSingleQuote: false,
+        quoteProps: "as-needed",
+        trailingComma: "all",
+        singleAttributePerLine: false,
+        htmlWhitespaceSensitivity: "css",
+        vueIndentScriptAndStyle: false,
+        proseWrap: "preserve",
+        insertPragma: false,
+        printWidth: 80,
+        requirePragma: false,
+        tabWidth: 2,
+        useTabs: false,
+        embeddedLanguageFormatting: "auto",
+        parser: "html",
+        prettierVersion: "2.8.7",
+        plugins: prettierPlugins,
+      },
     };
 
-    let formattedCode = prettier.format(value, prettierConfig);
+    let formattedCode = prettier.format(value, prettierConfig[type]);
 
     return formattedCode;
   };
@@ -61,90 +60,33 @@ const __script = async () => {
     provideDocumentFormattingEdits(model, options, token) {
       return [
         {
-          text: prettierFormat(model.getValue()), // put formatted text here
+          text: prettierFormat(model.getValue(), "js"), // put formatted text here
           range: model.getFullModelRange(),
         },
       ];
     },
   };
-  const languageId = "javascript";
+
+  const htmlFormatProvider = {
+    provideDocumentFormattingEdits(model, options, token) {
+      return [
+        {
+          text: prettierFormat(model.getValue(), "html"), // put formatted text here
+          range: model.getFullModelRange(),
+        },
+      ];
+    },
+  };
 
   monaco.languages.registerDocumentFormattingEditProvider(
-    languageId,
+    "javascript",
     javascriptFormatProvider
   );
 
-  // Fetches the script that will be injected into the document body
-
-  let element = document.querySelector(
-    `[aria-label="Open the execution log panel"]`
-  ).parentElement.parentElement.parentElement.parentElement;
-
-  let formatBtn = document.createElement("div");
-  formatBtn.setAttribute(
-    "style",
-    `font-family: "Google Sans",Roboto,Arial,sans-serif;
-    font-size: .875rem;
-    letter-spacing: .0107142857em;
-    font-weight: 500;
-    text-transform: none;
-    color: rgb(95,99,104);
-    cursor: pointer;
-    margin-left: 0.5rem;`
+  monaco.languages.registerDocumentFormattingEditProvider(
+    "html",
+    htmlFormatProvider
   );
-  formatBtn.innerText = "Format";
-
-  element.appendChild(formatBtn);
-
-  formatBtn.onclick = () => {
-    const files = document
-      .querySelector(`[aria-label="Project files"]`)
-      .querySelectorAll("li");
-
-    let filesObj = [];
-
-    files.forEach((file, index) => {
-      let fileName = file.innerText;
-      let fileClass = file.getAttribute("class");
-      filesObj.push({
-        fileName,
-        fileClassCount: fileClass.split(" ").length,
-        index,
-      });
-    });
-
-    filesObj.sort((a, b) => b.fileClassCount - a.fileClassCount);
-
-    let activeFileValue = monaco.editor.getModels()[0].getValue();
-
-    let prettierConfig = {
-      arrowParens: "always",
-      bracketSameLine: false,
-      bracketSpacing: true,
-      embeddedLanguageFormatting: "auto",
-      htmlWhitespaceSensitivity: "css",
-      insertPragma: false,
-      jsxSingleQuote: false,
-      printWidth: 80,
-      proseWrap: "preserve",
-      quoteProps: "as-needed",
-      requirePragma: false,
-      semi: true,
-      singleAttributePerLine: false,
-      singleQuote: false,
-      tabWidth: 2,
-      trailingComma: "es5",
-      useTabs: false,
-      vueIndentScriptAndStyle: false,
-      parser: "babel",
-      prettierVersion: "2.8.7",
-      plugins: prettierPlugins,
-    };
-
-    let formattedCode = prettier.format(activeFileValue, prettierConfig);
-
-    monaco.editor.getModels()[0].setValue(formattedCode);
-  };
 };
 
-__script();
+window.addEventListener("load", __script);
